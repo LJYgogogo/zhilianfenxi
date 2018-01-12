ea4e00801bd232e8e238db516c787bc292bb5ec0
LJYgogogo <35124652+LJYgogogo@users.noreply.github.com>
2018-01-12 10:54:25 +0800

    config
    设置参数

---
+++ config.py
@@ -0,0 +1,10 @@
+TOTAL_PAGE_NUMBER = 90 # PAGE_NUMBER: total number of pages，可进行修改
+
+KEYWORDS = ['数据分析师']  # 需爬取的关键字可以自己添加或修改
+
+# 爬取主要城市的记录
+ADDRESS = [ '北京', '上海', '广州', '深圳','天津', '武汉', '西安', '成都','杭州','长沙']
+# ADDRESS = ['全国']
+
+MONGO_URI = 'localhost'
+MONGO_DB = 'zhilian'
